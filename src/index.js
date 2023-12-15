b685c544d421c996f2d21eeb356820e97c75774a
Tran Thanh Dat <neban0444@gmail.com>
2023-12-15 17:26:07 +0700

    back-end

--- src/index.js
+++ src/index.js
@@ -3,49 +3,73 @@ import * as deepar from 'deepar';
 // Log the version. Just in case.
 console.log("Deepar version: " + deepar.version);
 
+const feetText = document.getElementById("feet-text");
 // Top-level await is not supported.
-// So we wrap the whole code in an async function that is called immediatly.
-(async function() {
+const selectedEffect = sessionStorage.getItem('selectedEffect');
 
-  // Resize the canvas according to screen size.
+// If there's a selected effect, initialize DeepAR with that effect
+if (selectedEffect) {
+  initializeDeepar(selectedEffect);
+}
+// So we wrap the whole code in an async function that is called immediatly.
+async function initializeDeepar(effectName) {
+  feetText.style.display = "none";
+  // Resize the canvas according to screen size. 
   const canvas = document.getElementById('deepar-canvas');
-  
-  // this is to make the canvas look good on high-res screens
   const scale = window.devicePixelRatio || 1;
-  
-  const width = window.innerWidth > window.innerHeight ? Math.floor(window.innerHeight * 0.66) : window.innerWidth
+  const width = window.innerWidth > window.innerHeight ? Math.floor(window.innerHeight * 0.66) : window.innerWidth;
   canvas.width = Math.floor(width * scale);
   canvas.height = Math.floor(window.innerHeight * scale);
-
-  // constrain the actual size of the canvas
   canvas.style.maxHeight = window.innerHeight + "px";
-  canvas.style.maxWidth = width + "px";;
-  
+  canvas.style.maxWidth = width + "px";
+
   // Initialize DeepAR.
   const deepAR = await deepar.initialize({
     licenseKey: 'b98a37541a356069cc7150029aad9ab89a62df4bc7de8f1b82689dd30a2ea69f638a76bb72a8c257',
     canvas: canvas,
-    effect: 'effects/Shoe', // The shoe-try-on effect file.
+    effect: `effects/${effectName}`, // The selected effect file.
     additionalOptions: {
-      // Use the front camera.
       cameraConfig: {
-        facingMode: "environment",
+        facingMode: "environment", // Use the front camera.
       },
-      hint: "footInit", // Add this hint to let DeepAR know we want to optimize for shoe-try-on use-case.
+      hint: "footInit",
     }
   });
 
   // Hide the loading screen.
   document.getElementById("loader-wrapper").style.display = "none";
 
-  // Register for a collback that lets you know when feet are detected.
+  // Register for a callback when feet are detected.
   deepAR.callbacks.onFeetTracked = (leftFoot, rightFoot) => {
     const feetText = document.getElementById("feet-text");
-    // Hide the text when the feet are first detected.
-    if(leftFoot.detected || rightFoot.detected) {
+    if (leftFoot.detected || rightFoot.detected) {
       feetText.style.display = "none";
-      deepAR.callbacks.onFeetTracked = undefined; // Unregister from the callback.
+      deepAR.callbacks.onFeetTracked = undefined;
     }
   };
 
-})();
+  return deepAR;
+}
+function getEffectNameFromCardId(cardId) {
+  // Example implementation: Return different effect names based on card IDs
+  return cardId;
+}
+
+// Perform actions when a product card is clicked
+function onProductCardClick(cardId) {
+  // Retrieve the effect name based on the selected card ID
+  const effectName = getEffectNameFromCardId(cardId);
+  sessionStorage.setItem('selectedEffect', effectName);
+  // Reload DeepAR with the selected effect
+  window.location.reload();
+}
+
+// Example of adding click event listeners to product cards
+const productCards = document.querySelectorAll('.product-card');
+productCards.forEach(card => {
+  card.addEventListener('click', function() {
+    const cardId = this.id; // Get the ID of the clicked card
+    onProductCardClick(cardId);
+  });
+});
+
